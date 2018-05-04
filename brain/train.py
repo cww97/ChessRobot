21c81a35d14ce20d93ad08d926a16593e6275a09
华贇 <huayun@huayundeMacBook-Air.local>
2018-05-04 23:54:13 +0800

    修改了下参数

--- brain/train.py
+++ brain/train.py
@@ -1,17 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-An implementation of the training pipeline of AlphaZero for Gomoku
-
-@author: Junxiao Song
-"""
-
 from __future__ import print_function
 import random
 import numpy as np
 from collections import defaultdict, deque
 from game import Board, Game
-#from policy_value_net import PolicyValueNet  # Theano and Lasagne
-# from policy_value_net_pytorch import PolicyValueNet  # Pytorch
 from policy_value_net_tensorflow import PolicyValueNet # Tensorflow
 from mcts_pure import MCTSPlayer as MCTS_Pure
 from mcts_alphaZero import MCTSPlayer
